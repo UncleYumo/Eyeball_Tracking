7153d4cf981a775c003ca0036d0eb2d8d7980be0
uncle_legion <wy13921971147@163.com>
2024-07-14 03:58:13 +0800

    完成了dlib提取人脸特征的功能

--- test/Dlib使用.py
+++ test/Dlib使用.py
@@ -2,3 +2,40 @@ import dlib
 import cv2
 import numpy as np
 
+cap = cv2.VideoCapture(0)
+
+# 加载dlib预训练的人脸检测器
+detector = dlib.get_frontal_face_detector()
+# 加载dlib预训练的人脸关键点检测器
+predictor = dlib.shape_predictor("E:\Dev_work\Python_Dev\Pycharm_Project\eye_track_study\shape_predictor_68_face_landmarks.dat")
+
+while True:
+    ret, frame = cap.read()
+    if ret is None:
+        break
+
+    # 转为灰度图
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # 使用detector进行人脸检测
+    faces = detector(gray, 0)   # 0表示使用默认的阈值, 1表示使用更高的阈值（1为上采样，0为下采样）
+
+    # 绘制矩形框
+    for face in faces:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.circle(frame, center=((x1+x2)//2, (y1+y2)//2), radius=2, color=(0, 0, 255), thickness=2)
+
+        # 使用predictor进行人脸关键点检测
+        shape = predictor(gray, face)
+        for i in range(68):
+            cv2.circle(frame, center=(shape.part(i).x, shape.part(i).y), radius=2, color=(255, 0, 0), thickness=2)
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(25) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ test/提取人脸特征点.py
@@ -0,0 +1,56 @@
+import os
+
+import dlib
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(1)
+# 项目根目录
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# 加载dlib预训练的人脸检测器
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(os.path.join(root_dir, "shape_predictor_68_face_landmarks.dat"))
+
+while True:
+    ret, frame = cap.read()
+    if ret is None:
+        break
+
+    # 转为灰度图
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # 使用detector进行人脸检测
+    faces = detector(gray, 0)   # 0表示使用默认的阈值, 1表示使用更高的阈值（1为上采样，0为下采样）
+    face_img = None
+
+    # 绘制矩形框
+    for face in faces:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 1)
+
+        # 计算人脸特征点
+        shape = predictor(gray, face)
+        for i in range(68):
+            x = shape.part(i).x
+            y = shape.part(i).y
+            cv2.circle(frame, (x, y), 2, (0, 0, 255), -1)
+            cv2.putText(frame, str(i), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.3,
+                        (255, 0, 255), 1)
+
+        # 裁剪人脸区域
+        face_img = frame[y1:y2, x1:x2]
+        # 显示裁剪出的人脸部分图像
+        if face_img is not None:
+            cv2.imshow("face", face_img)
+
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(25) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
